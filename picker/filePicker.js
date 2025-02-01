890035a7235a9b27b9e0efa04a4f84e3b32de4ea
YOLOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO <apid9755@gmail.com>
2025-02-01 08:29:45 +0800

    Add files via upload

---
+++ picker/filePicker.js
@@ -0,0 +1,10 @@
+import { SettingsManager } from "../../util.js";
+
+document
+    .getElementById("fileInput")
+    .addEventListener("change", async (event) => {
+        const file = event.target.files[0];
+        await SettingsManager.loadRemoteCDM(file).then(() => {
+            window.close();
+        });
+    });
